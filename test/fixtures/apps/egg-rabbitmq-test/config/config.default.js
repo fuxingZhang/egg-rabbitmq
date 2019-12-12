436263fff4ff64f53356cfbc5801ad42aee60ebe
star <502545703@qq.com>
2019-12-12 13:12:32 +0800

    update test and package.json

--- test/fixtures/apps/egg-rabbitmq-test/config/config.default.js
+++ test/fixtures/apps/egg-rabbitmq-test/config/config.default.js
@@ -1,17 +1,11 @@
 'use strict';
 
-// exports.rabbitmq = {
-//   enable: true,
-//   package: 'egg-rabbitmq',
-// };
 exports.rabbitmq = {
   enable: true,
   client: {
     url: 'amqp://guest:guest@localhost',
   },
-  // load into app, default is open
   app: true,
-  // load into agent, default is close
   agent: false,
 };
 
